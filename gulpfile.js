e7148d96051f43ffe4728eb826eb2639ffeed6af
Senchenko NG <notsu@mail.ru>
2017-02-19 20:50:42 +0300

    Create gulpfile and package.json

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp   = require('gulp'),
+    less   = require('gulp-less'),
+    concat = require('gulp-concat'),
+    del    = require('del');
+
+gulp.task('less-to-css', function() {
+  return gulp.src('less/*.less')
+             .pipe(concat('build.less'))
+             .pipe(less()) // convert to css using gulp-less
+             // .pipe(rename('build.css'))
+             .pipe(gulp.dest('public/css'))
+});
+
+// + JS (gulp-uglifyjs)
+// + img
+
+gulp.task('clean', function() {
+	return del.sync('public');
+});
+
+gulp.task('build', ['clean', 'less-to-css'], function() {
+  var build_fonts = gulp.src('fonts/**/*')
+	                      .pipe(gulp.dest('public/fonts'))
+
+  // var build_js = gulp.src('js/**/*')
+	//                    .pipe(gulp.dest('public/js'))
+
+  var build_html = gulp.src('*.html')
+	                     .pipe(gulp.dest('public'));
+})
+
+gulp.task('default', ['build']);
