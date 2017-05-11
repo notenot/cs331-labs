e7f6b376a60e70520e5d4c1a0f5775ce91ab6d16
Senchenko NG <notsu@mail.ru>
2017-05-11 17:18:24 +0300

    Add fade out for hamburger menu and hide feedback icon

--- js/partials/hamburger.js
+++ js/partials/hamburger.js
@@ -5,12 +5,14 @@ var headerContent = document
   .getElementsByClassName('header-content')[0];
 var menu = document
   .getElementsByClassName('navbar-burger-opened')[0];
+var popupFeedback = document.getElementsByClassName('popup-feedback')[0];
 var body = document.body;
 
 burger.addEventListener('click', function () {
   var opcty = 0;
 
   headerContent.style.display = 'none';
+  popupFeedback.style.display = 'none';
 
   menu.style.opacity = opcty;
   menu.style.display = 'flex';
@@ -33,9 +35,26 @@ burger.addEventListener('click', function () {
 });
 
 closeLink.addEventListener('click', function () {
-  menu.style.display = 'none';
+  var opcty = 1;
+  menu.style.opacity = opcty;
+
+  setTimeout(function fadeOut() {
+    opcty -= 0.2;
+    if (opcty < 0) {
+      opcty = 0;
+      menu.style.display = 'none';
+    }
+    menu.style.opacity = opcty;
+
+    if (opcty > 0) {
+      setTimeout(function () {
+        fadeOut();
+      }, 60);
+    }
+  }, 60);
 
   headerContent.style.display = 'flex';
+  popupFeedback.style.display = 'block';
 
   body.style.position = 'static';
   body.style.overflow = 'auto';
